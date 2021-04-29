3a57ea300e72fea224b3c0c6d9418130f0a39af2
Modabbir Tarique <43771389+modabbir24@users.noreply.github.com>
2021-04-30 02:35:29 +0530

    Add files via upload

--- script.py
+++ script.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request, send_from_directory
 from keras_preprocessing import image
 from keras.models import load_model
 import numpy as np
-import tensorflow as tf
 
 import numpy as np
 import matplotlib.pyplot as plt
